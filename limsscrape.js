1ea6f66a83f514181a4c12b472435eb7df61fb02
vzvenyach <vzvenyach@dccouncil.us>
2014-01-15 13:12:09 -0500

    alternate scraper fixed

--- limsscrape.js
+++ limsscrape.js
@@ -5,32 +5,24 @@ exports.get = function (lawNo) {
 
     //Go to the LIMS page 
     var browser = new Browser({ debug: false, runScripts: false });
-    browser.visit("http://dcclims1.dccouncil.us/lims/viewlegislation.aspx", function(callback) {
-
-        //Fill in the form
-        browser.fill("FullLegNo", lawNo).
-
-        //Press the "Search" button
-        pressButton("Search", function() {
-            //assert.ok(browser.success);
+    browser.visit("http://dcclims1.dccouncil.us/lims/legislation.aspx?LegNo=" + lawNo, function() {
 
             var fields = ["LegislationTitle","LegislationNo","ShortTitle","ActNoGI","LawNo","DateExpirationGI","DateEnactmentGI","DateEffectiveGI","DateIntroduction","PlaceIntroduction","DateCirculation","CommitteeReferral","Comments","OfficialReferral","DateReReferral","CommitteeReassign","CommentsReassign","DatePublicNotice","IntroducedBy","RequestedBy","CoSponsoredBy","DateCommitteeAction","ReportFiled","COWAction","DateFirstVote","DateFinalVote","DateThirdVote","DateReconsideration","DateTransmittedMayor","DateReviewEnd","DateSigned","Signature","DateReturned","ReturnedSignature","DateOverride","ActNo","DateEnactment","DateVeto","DateTransmittedCongress","DateReTransmitted","DateDCLaw","DatePublication","DCLawVol","DCLawPage","DCLaw","DCLawNo","DateEffective","DateApplicability","DateExpiration"];
 
             _.each(fields, function(f) {
             	var d = browser.query("#" + f);
-                out[d.id] = d.innerHTML;
+                out[f] = d.innerHTML;
             });
 
             //Additional metadata -- Documents
             var versions = browser.queryAll("A[id^=DocumentRepeater]");
             var v = []
 			_.each(versions, function (d) {
-                v.push('"' + d.innerHTML + '":"' + d.href + '"');
+                v.push(d.innerHTML + '":"' + d.href);
             });
             out["'versions'"] = v;
             console.log(JSON.stringify(out));
-        });
+        }
 
-    });
-	
+    )
 }
