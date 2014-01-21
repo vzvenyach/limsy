0d0c20fe3d17e5fe90f0a871f67118a387a7a385
vzvenyach <vzvenyach@dccouncil.us>
2014-01-21 07:52:13 -0500

    some more

--- getPDFs.py
+++ getPDFs.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python
 
 import json
+import re
+import urllib
 
 f = open('./out.json','r')
 measures = json.load(f) 
 
+f_log = open ('./pdfs/log.txt','w')
+
+bname = "http://dcclims1.dccouncil.us"
+
 for m in measures:
 	measure_id = m['LegislationNo']
 	for doc in m['versions']:
-		print "Measure No: " + measure_id + ", version: " + doc['version'] + ', url: ' + doc['url']
+		fname = './pdfs/' + measure_id + "_" + doc['version'] + '.pdf'
+		url = re.sub('\.\.', bname, doc['url'])
+		try:
+			urllib.urlretrieve(url,fname)
+			f_log.write(fname + ' added.')
+		except:
+			f_log("ERROR: There was an error here for:" + fname + "url: " + url)
+
 
 '''
-Sample output: "Measure No: B12-0144, version: Committee Report, url: ../images/00001/CP12/062324922_1.PDF"
-Naming convention: "B12-0144_version.pdf"
+Naming convention: "B12-0144_[version].pdf"
+'''
